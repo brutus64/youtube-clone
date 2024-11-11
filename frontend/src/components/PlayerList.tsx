32a0d00669dcdd3ec8f3b7c3711dbbf6055ab7d6
Ethan <ethan.b.chan@stonybrook.edu>
2024-11-11 16:39:20 -0500

    trying to do preload and added svgs

--- frontend/src/components/PlayerList.tsx
+++ frontend/src/components/PlayerList.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import VideoPlayer from "./VideoPlayer";
 import axios from "axios";
@@ -6,12 +6,37 @@ import axios from "axios";
 const PlayerList = () => {
     const { id } = useParams<{ id:string }>(); // ID of the first video
     const navigate = useNavigate();
-    const videoref = useRef({vidList:[""],ind:0});
+    const videoref = useRef({vidList:[{}],ind:0}); //vidList should now contain video info
+    const [ moreVideos, setMoreVideos ] = useState(false);
 
-    const fetchVideoId = async () => {  // Runs twice for some reason
-        const res = await axios.post("http://thewang.cse356.compas.cs.stonybrook.edu/api/videos",{count:1});
-        // //console.log(res)
-        return res.data.videos[0].id;
+
+    const fetchVideoId = async (num:number) => {  // Runs twice for some reason
+        const res = await axios.post("http://thewang.cse356.compas.cs.stonybrook.edu/api/videos",{count:num});
+        console.log(res)
+        // const dummyData = [{
+        //     id:1,
+        //     description:"hello",
+        //     title:"wassup",
+        //     watched:false,
+        //     liked:null,
+        //     likevalues:98
+        // },{
+        //     id:2,
+        //     description:"goodbye",
+        //     title:"im sad",
+        //     watched:true,
+        //     liked:true,
+        //     likevalues:63
+        // },{
+        //     id:3,
+        //     description:"i like bread",
+        //     title:"bread video",
+        //     watched:true,
+        //     liked:false,
+        //     likevalues:9
+        // }]
+        // return dummyData;
+        return res.data.videos;
     }
 
     const handleWheel = async (event:any) => {
@@ -20,7 +45,7 @@ const PlayerList = () => {
             // check if previous page exists
             if (videoref.current.ind != 0) {
                 videoref.current.ind -= 1;
-                navigate(`/play/${videoref.current.vidList[videoref.current.ind]}`);
+                navigate(`/play/${videoref.current.vidList[videoref.current.ind].id}`);
             }
         }
         else {
@@ -29,41 +54,49 @@ const PlayerList = () => {
             
             if (videoref.current.ind !== videoref.current.vidList.length-1) {
                 videoref.current.ind += 1;
-                navigate(`/play/${videoref.current.vidList[videoref.current.ind]}`);
+                navigate(`/play/${videoref.current.vidList[videoref.current.ind].id}`);
             }
-            else {// else load new random video and then navigate
-                const newid = await fetchVideoId();
-                videoref.current.vidList.push(newid);
+            else {// else recommend 10 more videos and then navigate
+                const newVideos = await fetchVideoId(10);
+                // videoref.current.vidList.push(newVideos)
+                videoref.current.vidList.push.apply(videoref.current.vidList,newVideos)
                 videoref.current.ind += 1;
                 //console.log("next ",nextState)
-                navigate(`/play/${newid}`);
+                navigate(`/play/${videoref.current.vidList[videoref.current.ind].id}`);
             }
-            
         }
     }
-
-    
-    
     useEffect(() => { //clear state on page reload
+
         console.log("reload")
-        videoref.current.vidList = [id];
+        videoref.current.vidList = [{
+            id:id,
+            description:"start video",
+            title:"potato",
+            watched:false,
+            liked:null,
+            likevalues:69
+        }];
+        fetchVideoId(9).then(res => {
+            videoref.current.vidList.push.apply(videoref.current.vidList,res);
+            setMoreVideos(true);
+        });
         videoref.current.ind = 0;
     }, []);
+    console.log("rerender")
     
     useEffect(() => {
-        //console.log("Event listener added")
         window.addEventListener("wheel", handleWheel);
         return () => {
-            //console.log("Event listener removed")
             window.removeEventListener("wheel", handleWheel);
         };
     }, []);
-    // //console.log("video list: ", vidList);
-    // //console.log("manifest Urls, ", manifestUrls);
-    //console.log(location);
+    // console.log("Rerender happened")
     return (
         <div className="video-list">
-            <VideoPlayer manifest={id} />
+            {videoref.current.vidList.map((vidData,i) => 
+                <VideoPlayer vidData={vidData} visible={i===videoref.current.ind}/>
+            )}
         </div>
     );
 };
