b624c8bc6e23244703c12effd2a614d720ff4e92
Ethan <ethan.b.chan@stonybrook.edu>
2024-11-11 01:38:20 -0500

    fixed type errors and some small changes

--- backend/src/rec.ts
+++ backend/src/rec.ts
@@ -1,7 +1,8 @@
 import { Gorse } from "gorsejs";
 import { db } from "./drizzle/db";
-import { video,view } from "./drizzle/schema";
+import { vid_like, video,view } from "./drizzle/schema";
 import { and, eq, inArray, notInArray, sql } from "drizzle-orm";
+import { title } from "process";
 
 const client = new Gorse({
     endpoint: "http://127.0.0.1:8088",
@@ -26,20 +27,45 @@ export function deleteRating(uid: string, vid: string, type: string) {
 }
 
 // return list of <num> video ids
-export async function recommend(uid: string, num: number) {
-    const recommendations = await client.getRecommend({
-        userId: uid,
+export async function recommend(uid: number, num: number) {
+    const recommendation_ids = await client.getRecommend({
+        userId: uid.toString(),
         cursorOptions: {n: num},
     });
+    // Response format:
+    // - id
+    // - description (video)
+    // - title (video)
+    // - watched (view)
+    // - liked (vid_like)
+    // - likevalues (video)
+    const recommendations:any = recommendation_ids.map(async id => {
+        const vidobj:any = await db.select({
+            id:video.id,
+            description:video.description,
+            title:video.title,
+            likevalues:video.like
+        }).from(video).where(eq(video.id,id));
+        vidobj.liked = null; //should be null, since recommendation system only uses new videos (user has not interacted)
+        vidobj.watched = false; //should be false, since recommendation system only used unwatched vids
+    })
 
     //not enough recommendations: insert random unwatched videos
     //does gorse already use as much videos as it can recommend (even bad recommendations)?
     if (recommendations.length < num) { 
-        const uidViews = await db.select({vid:view.video_id}).from(view).where(eq(view.user_id,uid));
-        const unwatched = await db.select({vid:video.id}).from(video).where(notInArray(video.id,uidViews));
-        const notReccAndUnwatched = unwatched.filter(vid => !recommendations.includes(vid));
+        const uidViews:any = (await db.select({vid:view.video_id}).from(view).where(eq(view.user_id,uid))).map(v => v.vid);
+        const unwatched = await db.select({
+            id:video.id,
+            description:video.description,
+            title:video.title,
+            likevalues:video.like
+        }).from(video).where(notInArray(video.id,uidViews));
+        const notReccAndUnwatched = unwatched.filter(vidobj => !recommendation_ids.includes(vidobj.id));
         
         for (let i = 0; i < notReccAndUnwatched.length; i++) {
+            const vidobj:any = notReccAndUnwatched[i];
+            vidobj.liked = null;
+            vidobj.watched = false;
             recommendations.push(notReccAndUnwatched[i]);
             if (recommendations.length == num)
                 break;
@@ -47,15 +73,30 @@ export async function recommend(uid: string, num: number) {
 
         //no more unwatched videos: insert watched videos
         if (recommendations.length < num) {
-            const watched = await db.select({vid:video.id}).from(video).where(inArray(video.id,uidViews)).limit(num-recommendations.length);
-            recommendations.push.apply(recommendations,watched);
+            const watched = await db.select({
+                id:video.id,
+                description:video.description,
+                title:video.title,
+                likevalues:video.like
+            }).from(video).where(inArray(video.id,uidViews)).limit(num-recommendations.length);
+            watched.forEach(async (vidobj:any) => {
+                vidobj.watched = true;
+                vidobj.liked = await db.select({liked:vid_like.liked}).from(vid_like).where(
+                    and(
+                        eq(vid_like.user_id,uid),
+                        eq(vid_like.video_id,vidobj.id)
+                    )
+                )
+                recommendations.push(vidobj);
+            })
+            // recommendations.push.apply(recommendations,watched);
         }
 
         // if there are still less than <num> videos in recommendation
         // array at this point, append videos already in the array
         while (recommendations.length < num) {
             let randInd = Math.floor(Math.random() * recommendations.length);
-            recommendations.push(recommendations[randInd]);
+            recommendations.push({...recommendations[randInd]});
         } //this will infinite loop if there are zero videos in the system
     }
     return recommendations;
